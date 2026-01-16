24301e9f8e2f874aa483132b0af4cb08108d1d18
nandini.korukonda-coditas <nandini.korukonda@coditas.com>
2026-01-16 12:40:09 +0530

    Enhanced UI

--- src/app/features/dashboard/overview/overview.ts
+++ src/app/features/dashboard/overview/overview.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RecipeStore } from '../../../core/stores/recipe-store';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar'; 
 import { Card } from '../../../shared/components/card/card';
 import { Table } from '../../../shared/components/table/table';
 import { Button } from '../../../shared/components/button/button';
@@ -9,7 +10,7 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-overview',
-  imports: [Card, Table,Button,MatIconModule,CommonModule],
+  imports: [Card, Table,Button,MatIconModule,CommonModule,MatSnackBarModule],
   templateUrl: './overview.html',
   styleUrl: './overview.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,16 +18,34 @@ import { CommonModule } from '@angular/common';
 export class Overview {
 
   store=inject(RecipeStore); 
-  favStore=inject(FavouriteStore)  
+  favStore=inject(FavouriteStore) 
+  toast = inject(MatSnackBar);
 
   editRecipe(recipe: any) {
     console.log('Edit recipe', recipe);
-    this.store.updateRecipe(recipe);
+    // this.store.updateRecipe(recipe);
+
+    // ✅ Green toast for successful update
+    this.toast.open('Recipe updated successfully!', 'Close', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: ['success-snackbar'],
+    });
   }
-  
+
   deleteRecipe(id: number) {
     this.store.deleteRecipe(id);
+
+    // ✅ Red toast for delete
+    this.toast.open('Recipe deleted successfully!', 'Close', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: ['error-snackbar'],
+    });
   }
+   
 
 
 }
